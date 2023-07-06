221f0c6cd2423505886a79f30a22d23441582db0
SamsunNaher-1604048 <samsun.naher.nishi.2017@gmail.com>
2023-07-06 08:49:17 +0600

    6.7.2023

--- src/component/Counter.js
+++ src/component/Counter.js
@@ -2,13 +2,25 @@ import React, { useState } from 'react'
 
 const Counter = () => {
     const [count,setCount] = useState(0)
+    const onIncrement = (count) =>{
+      count=count+1;
+      setCount(count);
+    }
+
+    const onDecrement=(count)=>{
+      count=count-1;
+       setCount(count);
+    }
+
   return (
-    <div class="flex justify-center flex-col">
-      <h1>Counter App</h1>
-      <div>{count}</div>
-      <div>
-        <button>Inctenent</button>
-        <button>Decrement</button>
+    <div class="flex justify-center mt-10">
+      <div class = "box-border h-32 w-72 bg-pink-100 text-center">
+        <h1 >Counter App</h1>
+        <div>{count}</div>
+        <div class="flex justify-center space-x-4">
+          <button onClick={()=>(onIncrement(count))}>Inctenent</button>
+          <button onClick={()=>(onDecrement(count))}>Decrement</button>
+        </div>
       </div>
     </div>
   )
